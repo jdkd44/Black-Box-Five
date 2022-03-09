8bf6a9fd3778c9a817e06192b61c0febd8b15674
japghb <japghb@mst.edu>
2022-03-09 12:35:16 -0600

    Added some sensor reading code and updated libraries that needed updating

--- Code/sensorRead.py
+++ Code/sensorRead.py
@@ -1,3 +1,28 @@
+import time
+#import board
+import busio
+
+import adafruit_gps
+import adafruit_mpu6050
+import adafruit_bmp3xx
+
+#Creates I2C interface to communicate using pins
+#i2c = board.I2C()
+
+#Create a BMP module instance
+bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
+
+#Create a MPU module instance
+#mpu = adafruit_mpu6050.MPU6050(i2c)
+
+#Create a GPS module instance
+#gps = adafruit_gps.GPS_GtopI2C(i2c, debug=False)
+
+#Initialize GPS module by turning on GGA and RMC info
+#gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+
+#Set update rate to once a second
+#gps.send_command(b"PMTK220, 1000") 
 
 #gather sensor data into one location
 def dbData():
@@ -9,13 +34,20 @@ def dbData():
     dataMax = 3
     dataMin = -3    
     lateral_acc = randint(dataMin, dataMax)
+    #lateral_acc = mpu.accel_x
     vertical_acc = randint(dataMin, dataMax)
+    #vertical_acc = mpu.accel_y
     vel = randint(dataMin, dataMax)
     height = randint(dataMin, dataMax)
+
+    #altitude in meters based on sea level pressure
+    #height = bmp.altitude 
 
     return lateral_acc, vertical_acc, vel, height
 
 def gpsCoordinates():
+    #gps_lat = gps.latitude
+    #gps_lon = gps.longtidue
 
     #TESTING ONLY
     from random import randint
